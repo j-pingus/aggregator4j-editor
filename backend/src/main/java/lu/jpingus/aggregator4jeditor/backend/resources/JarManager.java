af68ddbb6751d5562b9817559177ba3bffb8ca08
gerald <gerald.even@gmail.com>
2020-04-03 21:38:47 +0200

    web application initialization

---
+++ backend/src/main/java/lu/jpingus/aggregator4jeditor/backend/resources/JarManager.java
@@ -0,0 +1,44 @@
+package lu.jpingus.aggregator4jeditor.backend.resources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController()
+@RequestMapping("/jar")
+public class JarManager {
+    @Value("${editor.jarfolder}")
+    String jarFolderPath;
+
+    @GetMapping
+    public List<String> getAllJars() {
+        File jarFolder = getOrCreateJarFolder();
+        return Arrays.stream(
+                jarFolder.listFiles(pathname -> pathname.getName().endsWith(".jar")))
+                .map(file -> file.getName())
+                .collect(Collectors.toList());
+    }
+
+    private File getOrCreateJarFolder() {
+        if (StringUtils.isEmpty(jarFolderPath)) {
+            jarFolderPath = "my-jar-folder";
+        }
+        File ret = new File(jarFolderPath);
+        if (ret.exists() && ret.isDirectory()) return ret;
+        if (ret.exists()) throw new Error(ret + " is not a folder");
+        if (ret.getAbsoluteFile().getParentFile().canWrite()) {
+            ret.mkdirs();
+            return ret;
+        }
+        throw new Error(ret + " could not be created");
+
+    }
+
+}
---
+++ frontend/src/main/angular/src/app/side-menu/side-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+  @Input()
+  title:String
+  
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
