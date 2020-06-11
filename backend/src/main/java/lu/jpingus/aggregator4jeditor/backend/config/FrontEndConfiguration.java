26167317fa538e173b6d82501bfa4012906f9224
Gerald Even <Gerald.Even@arhs-cube.Com>
2020-06-11 11:32:38 +0200

    Build and serve front end
    - mvn clean install produces the angular compiled code (frontend)
    - frontend imported in backend
    - spring configured to serve files from classpath:/static

--- backend/src/main/java/lu/jpingus/aggregator4jeditor/backend/config/FrontEndConfiguration.java
+++ backend/src/main/java/lu/jpingus/aggregator4jeditor/backend/config/FrontEndConfiguration.java
@@ -27,5 +27,4 @@ public class FrontEndConfiguration implements WebMvcConfigurer {
                                 : new ClassPathResource("/static/index.html");
                     }
                 });
-    }
-}
+    }}
