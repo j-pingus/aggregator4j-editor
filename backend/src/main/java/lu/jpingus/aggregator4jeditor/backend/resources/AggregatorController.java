7e5189764dff5db9f4c24a921439d593e8d43638
Gérald Even <gerald.even@arhs-cube.com>
2022-02-03 13:21:51 +0100

    add specifics to AF, jackson datatypes and variables

--- backend/src/main/java/lu/jpingus/aggregator4jeditor/backend/resources/AggregatorController.java
+++ backend/src/main/java/lu/jpingus/aggregator4jeditor/backend/resources/AggregatorController.java
@@ -2,6 +2,8 @@ package lu.jpingus.aggregator4jeditor.backend.resources;
 
 import com.crabshue.commons.aggregator.AggregatorContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import eu.europa.ec.eac.af.form_service.forms_service.forms_plugin_common.computations.aggregation.constants.AggregationConstants;
 import lu.jpingus.aggregator4jeditor.backend.model.ExecutionTrace;
 import lu.jpingus.aggregator4jeditor.backend.model.Project;
 import lu.jpingus.aggregator4jeditor.backend.services.ClassLoaderService;
@@ -11,9 +13,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import static com.crabshue.commons.aggregator.StringFunctions.isEmpty;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.springframework.util.StringUtils.isEmpty;
+
 @RestController
 @RequestMapping(value = "/api/v1/aggregator", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AggregatorController {
@@ -23,22 +27,31 @@ public class AggregatorController {
     String jarFolderPath;
 
     @PutMapping(value = "evaluate", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Object evaluate(@RequestBody Project project, @RequestParam(required = false) String expression) throws IOException{
-        ObjectMapper mapper = new ObjectMapper();
-        File jarFile = new File(jarFolderPath, project.getJarName());
-        Class<?> modelClass = service.loadClass(jarFile, project.getClassName());
-        Object o = mapper.reader().forType(modelClass).readValue(project.getJsonPayload());
-        AggregatorContext context = AggregatorContext.builder()
-                .config(project.getConfiguration())
-                .classLoader(service.getClassLoader(jarFile))
-                .debug(true)
-                .build();
+    public Object evaluate(@RequestBody Project project,
+                           @RequestParam(required = false) String expression)
+            throws Exception {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            File jarFile = new File(jarFolderPath, project.getJarName());
+            Class<?> modelClass = service.loadClass(jarFile, project.getClassName());
+            Object o = mapper.reader().forType(modelClass).readValue(project.getJsonPayload());
+            AggregatorContext context = AggregatorContext.builder()
+                    .config(project.getConfiguration())
+                    .classLoader(service.getClassLoader(jarFile))
+                    .debug(true)
+                    .build();
+            context.addVariable(AggregationConstants.ZERO_BD, BigDecimal.ZERO);
+            context.addVariable(AggregationConstants.ZERO_BI, BigInteger.ZERO);
+            return ExecutionTrace.builder()
+                    .result(context.process(o))
+                    .evaluated(isEmpty(expression) ? null : context.evaluate(expression))
+                    .trace(context.getLastProcessTrace()).build();
+        } catch (Throwable e) {
+            throw new Exception("Could not evaluate:" + e.getMessage(), e);
 
-        return ExecutionTrace.builder()
-                .result(context.process(o))
-                .evaluated(isEmpty(expression)?null:context.evaluate(expression))
-                .trace(context.getLastProcessTrace()).build();
+        }
+
     }
-
 }
 
